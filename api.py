2c50f881606ac610471082a016e8ee562a24c0a4
Valentin <valentin.gourmet@gmail.com>
2021-07-06 12:11:41 +0200

    trained svc, build api to use it and start of documentation

---
+++ api.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from typing import Optional, Dict, List
+from pydantic import BaseModel
+from joblib import load
+import pandas as pd
+import uvicorn
+
+app = FastAPI()
+
+trained_svc = load("svc.joblib")
+
+
+class NewRequest(BaseModel):
+    """
+    Request should arrive as a dict of {feature name: value}
+    Name is just to identify the request
+    """
+    name: str = ""
+    features: Dict[str, float]
+
+
+class ResponseBody(BaseModel):
+    """
+    Response body for a predictions request: a Dict of metrics messages and their values
+    """
+    output: Optional[Dict[str, int]]
+    name: Optional[str] = None
+    error: Optional[str] = None
+
+
+@app.get("/")
+def read_root():
+    return "You can access you credit card fraud detector at the /predict endpoint"
+
+
+@app.get("/testing_out_stuff/{user_id}/{user_age}")
+def testing_fastapi(user_id: str = "SomeID", user_age: int = 20) -> str:
+    return f"Your ID is {user_id} and your age is {user_age}"
+
+
+@app.post("/predict")
+def predict_fraud(card_transaction: NewRequest):
+    input_features = card_transaction.features
+    name = card_transaction.name
+    input_features = pd.DataFrame.from_dict(
+        {k: [v] for k, v in input_features.items()})
+    pred = trained_svc.predict(input_features)[0]
+    return ResponseBody(output={"Prediction is:": pred}, name=name)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ request.py
@@ -0,0 +1,55 @@
+import requests
+import json
+
+root_url = "http://127.0.0.1:8000/"
+
+test_input_fraud = {"features": {'Time': 472.0,
+                                 'V1': -3.0435406239976,
+                                 'V2': -3.15730712090228,
+                                 'V3': 1.08846277997285,
+                                 'V4': 2.2886436183814,
+                                 'V5': 1.35980512966107,
+                                 'V6': -1.06482252298131,
+                                 'V7': 0.325574266158614,
+                                 'V8': -0.0677936531906277,
+                                 'V9': -0.270952836226548,
+                                 'V10': -0.838586564582682,
+                                 'V11': -0.414575448285725,
+                                 'V12': -0.503140859566824,
+                                 'V13': 0.676501544635863,
+                                 'V14': -1.69202893305906,
+                                 'V15': 2.00063483909015,
+                                 'V16': 0.666779695901966,
+                                 'V17': 0.599717413841732,
+                                 'V18': 1.72532100745514,
+                                 'V19': 0.283344830149495,
+                                 'V20': 2.10233879259444,
+                                 'V21': 0.661695924845707,
+                                 'V22': 0.435477208966341,
+                                 'V23': 1.37596574254306,
+                                 'V24': -0.293803152734021,
+                                 'V25': 0.279798031841214,
+                                 'V26': -0.145361714815161,
+                                 'V27': -0.252773122530705,
+                                 'V28': 0.0357642251788156,
+                                 'Amount': 529.0},
+                    "name": "Customer 10"}
+
+
+response_body1 = requests.get(root_url)
+
+print(response_body1.text)
+
+response_body2 = requests.get(root_url + "testing_out_stuff/val111/24")
+
+print(response_body2.text)
+
+response_body3 = requests.post(root_url + "predict", json=test_input_fraud)
+
+print(response_body3.text)
+
+with open('test_data.json') as json_file:
+    data = json.load(json_file)
+
+response_body4 = requests.post(root_url + "predict", json=data)
+print(response_body4.text)
